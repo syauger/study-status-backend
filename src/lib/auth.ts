a55797d9110cb46c3504e1d68e6c3780558aba59
sam <syauger@121g.io>
2026-09-15 17:41:50 -0400

    auth lib use new schema struct

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,19 @@
 import { betterAuth } from "better-auth/minimal";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { drizzle } from "drizzle-orm/d1";
+import * as appSchema from "@/db/app.schema";
+import * as authSchema from "@/db/auth.schema";
+
+const schema = {
+  ...appSchema,
+  ...authSchema,
+};
 
 export const auth = (env: Cloudflare.Env) => {
   const db = drizzle(env.DB);
 
   return betterAuth({
-    database: drizzleAdapter(db, { provider: "sqlite" }),
+    database: drizzleAdapter(db, { provider: "sqlite", schema }),
     baseURL: env.BETTER_AUTH_URL,
     secret: env.BETTER_AUTH_SECRET,
     emailAndPassword: {
