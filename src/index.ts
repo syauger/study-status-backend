c71642681c5e69dde8f8ed3d142a6dfa123d6b22
sam <syauger@121g.io>
2026-09-15 17:41:42 -0400

    initial app config and setup

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,20 @@
 import { fromHono } from "chanfana";
 import { Hono } from "hono";
-import { TaskCreate } from "./endpoints/taskCreate";
-import { TaskDelete } from "./endpoints/taskDelete";
-import { TaskFetch } from "./endpoints/taskFetch";
-import { TaskList } from "./endpoints/taskList";
 import { auth } from "./lib/auth";
+import { reportsEndpoints } from "@/endpoints/reports";
 
-// Start a Hono app
 const app = new Hono<{
   Bindings: Cloudflare.Env;
 }>();
 
 app.all("/api/auth/*", (c) => auth(c.env).handler(c.req.raw));
 
-app.get("/api/test", (c) => c.text("Hello from Hono!"));
+const openapi = fromHono(app, {
+  docs_url: "/",
+});
 
-// // Setup OpenAPI registry
-// const openapi = fromHono(app, {
-//   docs_url: "/",
-// });
-//
-// // Register OpenAPI endpoints
-// openapi.get("/api/tasks", TaskList);
-// openapi.post("/api/tasks", TaskCreate);
-// openapi.get("/api/tasks/:taskSlug", TaskFetch);
-// openapi.delete("/api/tasks/:taskSlug", TaskDelete);
+openapi.get("/api/reports", reportsEndpoints.list);
+openapi.post("/api/reports", reportsEndpoints.create);
+openapi.get("/api/reports/:reportId", reportsEndpoints.get);
 
-// You may also register routes for non OpenAPI directly on Hono
-// app.get('/test', (c) => c.text('Hono!'))
-
-// Export the Hono app
 export default app;
