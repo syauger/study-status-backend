2f8493672bcabf41c593782087c29f1788970355
sam <syauger@121g.io>
2026-09-15 17:40:55 -0400

    multi-part schema

--- better-auth.config.ts
+++ better-auth.config.ts
@@ -1,13 +1,20 @@
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { betterAuth } from "better-auth/minimal";
 import { drizzle, DrizzleD1Database } from "drizzle-orm/d1";
+import * as appSchema from "./src/db/app.schema";
+import * as authSchema from "./src/db/auth.schema";
+
+const schema = {
+  ...appSchema,
+  ...authSchema,
+};
 
 const { DB, BETTER_AUTH_URL, BETTER_AUTH_SECRET } = process.env;
 
 const db = drizzle(DB as unknown as DrizzleD1Database);
 
 export const auth = betterAuth({
-  database: drizzleAdapter(db, { provider: "sqlite" }),
+  database: drizzleAdapter(db, { provider: "sqlite", schema }),
   baseURL: BETTER_AUTH_URL,
   secret: BETTER_AUTH_SECRET,
 });
