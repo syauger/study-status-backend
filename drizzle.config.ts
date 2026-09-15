9969d986790b8ab6425bc45cd14511a9ce77a969
sam <syauger@121g.io>
2026-09-15 17:42:28 -0400

    use new schema struct in drizzle conf

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/db/schema.ts",
+  schema: "./src/db/*.schema.ts",
   dialect: "sqlite",
 });
